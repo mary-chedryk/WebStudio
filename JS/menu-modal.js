a93620a92b0305126abb2cdb278f5b85fe99c529
Mariia Chedryk <mary.chedryk@gmail.com>
2024-06-12 17:59:17 -0400

    mobile menu

---
+++ JS/menu-modal.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("myModal-menu");
+
+var btn = document.getElementById("myBtn-menu");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  modal.style.visibility = "visible";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
